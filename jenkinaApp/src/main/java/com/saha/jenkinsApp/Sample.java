ed0424bd7a733301f5505069760eabc5a208e04a
bsaha <bee.saha@yahoo.com>
2020-03-27 12:49:57 +0530

    jenkinsApp commit

---
+++ jenkinaApp/src/main/java/com/saha/jenkinsApp/Sample.java
@@ -0,0 +1,28 @@
+package com.saha.jenkinsApp;
+
+public class Sample {
+
+	/**
+	 * 
+	 */
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static int staticAdd(int x, int y) {
+		return x + y;
+	}
+
+	public int add(int x, int y) {
+		return x + y;
+	}
+
+	private int privateAdd(int x, int y) {
+		return x + y;
+	}
+
+	public int publicAdd(int x, int y) {
+		return privateAdd(x, y);
+	}
+
+}
