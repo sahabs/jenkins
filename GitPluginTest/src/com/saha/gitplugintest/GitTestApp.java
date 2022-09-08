a91c065b7dd3b1a596ac563e3b6bb22621b0ed84
bsaha <bee.saha@yahoo.com>
2022-09-09 03:14:19 +0530

    ubuntu20FirstCommit: first commit

---
+++ GitPluginTest/src/com/saha/gitplugintest/GitTestApp.java
@@ -0,0 +1,11 @@
+package com.saha.gitplugintest;
+
+public class GitTestApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hell World!");
+		System.out.println("Eclipse Git Plugin Test!");
+
+	}
+
+}
