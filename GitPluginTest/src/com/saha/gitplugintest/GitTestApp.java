c876a2eb08f0a73bbf5a1cbdc1f54e702c3327cc
bsaha <bee.saha@yahoo.com>
2022-09-09 03:41:47 +0530

    ubunu20SecondCommitGitPluginTest: second commit for gitPLuginTest on master branch. better have a separate branch for each project.

--- GitPluginTest/src/com/saha/gitplugintest/GitTestApp.java
+++ GitPluginTest/src/com/saha/gitplugintest/GitTestApp.java
@@ -3,8 +3,8 @@ package com.saha.gitplugintest;
 public class GitTestApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hell World!");
-		System.out.println("Eclipse Git Plugin Test!");
+		System.out.println("Hello World!Eclipse Git Plugin Test!");
+		System.out.println("the previous first commit was for GitPluginTest project.");
 
 	}
 
